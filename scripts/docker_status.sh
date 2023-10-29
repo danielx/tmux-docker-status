a5741590ba2a3f03c83cb1df8a48563a8c2bce27
Daniel Liljeström <daniel@mailbox.sh>
2023-10-29 13:14:07 +0100

    add arm mac support and change default icon

--- scripts/docker_status.sh
+++ scripts/docker_status.sh
@@ -1,36 +1,27 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$CURRENT_DIR/helpers.sh"
 
 docker_option_string="@docker_icon"
 
-docker_icon_osx="⧈"
-docker_icon="DOCKERS: "
-
-docker_icon_default() {
-	if is_osx; then
-		echo "$docker_icon_osx"
-	else
-		echo "$docker_icon"
-	fi
-}
+docker_icon=""
 
 docker_count() {
 	CONTAINERS="-1"
 	if is_osx; then
 		if command_exists "docker"; then
-			if [[ $((`ps -ef | grep com.docker.hyperkit | grep -v grep | wc -l`)) > 0 ]]; then
-				CONTAINERS=$((`docker ps -q | wc -l`))
+			if [[ $(ps -ef | grep -c 'com.docker.docker') -gt 1 ]]; then
+				CONTAINERS=$(docker ps -q | wc -l | tr -d ' ')
 			fi
 		fi
 	fi
 
 	if is_linux; then
 		if command_exists "docker"; then
-			if [[ $((`ps -ef | grep dockerd | grep -v grep | wc -l`)) > 0 ]]; then
-				CONTAINERS=$((`docker ps -q | wc -l`))
+			if [[ $(ps -ef | grep dockerd) -gt 1 ]]; then
+				CONTAINERS=$(docker ps -q | wc -l | tr -d ' ')
 			fi
 		fi
 	fi
@@ -40,9 +31,8 @@ docker_count() {
 
 print_docker_status() {
 	# spacer fixes weird emoji spacing
-	local spacer=" "
 	if [[ $(docker_count) -ge 0 ]]; then
-		printf "$(get_tmux_option "$docker_option_string" "$(docker_icon_default)")$spacer$(docker_count)"
+		printf "$(get_tmux_option "$docker_option_string" "$docker_icon") $(docker_count)"
 	fi
 }
 
